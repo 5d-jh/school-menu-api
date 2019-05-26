45fc43fb602297a74cff6bb2e34c5bc24d0f81d9
이정현 <subacc09@daum.net>
2019-05-27 01:15:45 +0900

    저장 플랫폼 변경
    S3 -> DynamoDB

--- api/responseCache.js
+++ api/responseCache.js
@@ -2,7 +2,7 @@ const process = require('process');
 const AWS = require('aws-sdk');
 
 const NODE_ENV = process.env.NODE_ENV;
-const Bucket = NODE_ENV === 'development' ? 'school-menu-api-dev' : 'school-menu-api-caches';
+const TableName = NODE_ENV === 'development' ? 'SchoolMenu_dev' : 'SchoolMenu';
 
 
 // *** AWS Lambda 가 아닌 환경에서 실행할 경우 다음 코드의 주석을 해제합니다. ***
@@ -12,7 +12,7 @@ const Bucket = NODE_ENV === 'development' ? 'school-menu-api-dev' : 'school-menu
 //   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
 // });
 
-const S3 = new AWS.S3();
+const docCli = new AWS.DynamoDB.DocumentClient({ apiVersion: '2012-08-10' });
 
 const getMenu = require('./getMenu');
 
@@ -37,21 +37,31 @@ class ResponseCache {
     return this.menuYear.toString()+this.menuMonth.toString()+this.schoolCode+'.json';
   }
 
-  cacheMenu(schoolMenu) {
+  async cacheMenu(schoolMenu) {
     const params = {
-      Bucket,
-      Key: this.getObjKey(this.schoolCode, this.menuYear, this.menuMonth),
-      Body: JSON.stringify({ schoolMenu, isCached: true })
+      TableName,
+      Item: {
+        'MenuYM': `${this.menuYear}.${this.menuMonth}`,
+        'SchoolCode': this.schoolCode,
+        'SchoolMenu': schoolMenu
+      }
     };
-    S3.putObject(params).promise()
-    .catch(err => err);
+    try {
+      await docCli.put(params, (err, data) => {
+        if (err) console.error(err);
+        else console.log(data);
+      });
+    } catch (err) {
+      throw err;
+    }
+    
   }
 
   async getMenuThenCache() {
     try {
       const schoolMenu = await getMenu(this.schoolType, this.schoolCode, this.menuYear, this.menuMonth);
 
-      if (schoolMenu.hasData) {
+      if (schoolMenu && schoolMenu.hasData) {
         this.cacheMenu(schoolMenu.menu);
       }
 
@@ -63,38 +73,35 @@ class ResponseCache {
   }
 
   async require() {
-    try {
-      const params = {
-        Bucket,
-        Key: this.getObjKey(this.schoolCode, this.menuYear, this.menuMonth)
-      };
-      const schoolMenuData = await S3.getObject(params).promise()
-      .then(data => JSON.parse(data.Body));
-
-      return schoolMenuData; //returns { menu, isCached }
-
-    } catch(err) {
-      if (err.code === 'NoSuchKey') {
-        //오브젝트가 존재하지 않는 경우
-        if (this.willStore()) {
-          //과거 또는 현재 식단을 요쳥한 경우
-          return {  
-            schoolMenu: await this.getMenuThenCache(),
-            isCached: false
-          };
-        }
-
-        //미래 식단을 요청한 경우
-        const { menu } = await getMenu(this.schoolType, this.schoolCode, this.menuYear, this.menuMonth);
-        return {
-          schoolMenu: menu,
-          isCached: false
-        };
-        
-      } else {
-        throw err;
+    const params = {
+      TableName,
+      Key: {
+        'MenuYM': `${this.menuYear}.${this.menuMonth}`,
+        'SchoolCode': this.schoolCode,
       }
+    };
+    const ddbResult = await docCli.get(params).promise()
+    .catch(err => err);
+
+    if (ddbResult.Item === undefined) { //오브젝트가 존재하지 않는 경우
+      const isCached = false;
+
+      if (this.willStore()) { //과거 또는 현재 식단을 요쳥한 경우
+        return {  
+          schoolMenu: await this.getMenuThenCache(),
+          isCached
+        };
+      }
+
+      //미래 식단을 요청한 경우
+      const { menu } = await getMenu(this.schoolType, this.schoolCode, this.menuYear, this.menuMonth);
+      return {
+        schoolMenu: menu,
+        isCached
+      };
     }
+
+    return { schoolMenu: ddbResult.Item.SchoolMenu, isCached: true }; //returns { menu, isCached }
   }
 }
 
