a14c3ec76ff2ab6bbc8531f144993f923a6ad2cb
5d-jh <jeonghyun0126@outlook.com>
2020-04-14 22:08:36 +0900

    패키지 정리

--- functions/package-function/src/index.ts
+++ functions/package-function/src/index.ts
@@ -1,9 +1,10 @@
-import * as functions from 'firebase-functions';
-import { msg } from 'package-common'
+import { https } from "firebase-functions";
+import { initializeApp } from "firebase-admin";
+import { schoolMenuApp } from "package-school-menu"
+
+initializeApp();
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-export const api = functions.https.onRequest((request, response) => {
-    response.send(msg);
-});
+export const api = https.onRequest(schoolMenuApp);
