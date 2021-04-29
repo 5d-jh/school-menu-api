b4269aa0bdd892a88183362e7c994badce7c03fe
5d-jh <jeonghyun0126@outlook.com>
2021-04-30 03:05:55 +0900

    테스트 추가

---
+++ functions/package-function/test/test.ts
@@ -0,0 +1,20 @@
+import supertest from 'supertest';
+import app from '../src/index.local';
+import { notStrictEqual } from 'assert';
+
+describe('[Integration] school-menu', function () {
+  let request: supertest.SuperTest<supertest.Test>;
+
+  this.timeout(50000);
+
+  this.beforeAll(() => {
+    request = supertest(app);
+  });
+
+  it('Issue #41', async () => {
+    const { body } = await request.get('/api/high/B100002365?year=2019&month=05&date=08')
+      .expect(200);
+
+    notStrictEqual(body.menu.length, 0);
+  });
+});
